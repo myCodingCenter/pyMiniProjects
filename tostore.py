600b0afac871b2660f809681b6023b5c4bda0614
rahul <cccontactme1@gmail.com>
2024-04-10 03:55:03 -0700

    creating multiplicationTable() function

--- tostore.py
+++ tostore.py
@@ -1,1 +1,16 @@
+import os
 print("Hello World")
+
+def multiplicationTable(n):
+    os.system("cls")
+    print("\n")
+    print("-----------------Multiplication Table for",n,"-----------")
+    print()
+    for i in range(1,10+1):
+        print(f"\t\t{n} X {i} = {i*n}")
+
+    print()
+
+num = int(input("Enter number : "))
+
+multiplicationTable(num)
